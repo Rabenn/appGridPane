d7c7f50c2b89e67e88896397d759db2e87d08f41
Raben <rubenxioluna@gmail.com>
2025-09-19 14:15:08 +0200

    FIX: Arreglo en los controllers

---
+++ src/main/java/es/ruben/GridController.java
@@ -0,0 +1,33 @@
+package es.ruben;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class GridController {
+
+    @FXML
+    private Button botonCancel;
+
+    @FXML
+    private Button botonOK;
+
+    @FXML
+    private Label labelDescription;
+
+    @FXML
+    private Label labelNombre;
+
+    @FXML
+    private Label labelStatus;
+
+    @FXML
+    private TextArea textareaDescripcion;
+
+    @FXML
+    private TextField textfieldNombre;
+
+}
