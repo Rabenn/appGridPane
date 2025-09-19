11f3ac44b30bbbb59f32f4deffb61132b6f453f3
Raben <rubenxioluna@gmail.com>
2025-09-19 17:23:29 +0200

    CHORE: Ajustes en el codigo de App.java y del pom

--- src/main/java/es/ruben/App.java
+++ src/main/java/es/ruben/App.java
@@ -5,34 +5,43 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
 /**
- * JavaFX App
+ * Clase principal que carga la interfaz de usuario desde un archivo FXML.
+ * La estructura y los estilos están definidos en App.fxml y styles.css.
  */
 public class App extends Application {
 
-    private static Scene scene;
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+
+
+        logger.info("Lanzador lanzado");
+
+        // Leemos con FXMLLoader y a partir de ahi construimos los objetos
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/app_info.fxml"));
+
+        Parent root = loader.load();
+
+        // Crea la escena desde el FXML.
+        Scene scene = new Scene(root);
+        logger.info("entra al css");
+        scene.getStylesheets().add(getClass().getResource("css/estilos.css").toExternalForm());
+        logger.info("funciona css");
+
+        // Muestra la ventana (Stage).
+        stage.setTitle("Aplicación sencilla con GridPane");
         stage.setScene(scene);
         stage.show();
     }
-
-    static void setRoot(String fxml) throws IOException {
-        scene.setRoot(loadFXML(fxml));
-    }
-
-    private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
-    }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
